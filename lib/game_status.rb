fe4a5400175cc03ed9e5b15505fa47f3cca0c78a
Javier Huerta Jr <javsteratii@gmail.com>
2019-09-08 18:55:46 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -12,12 +12,12 @@ end
 
 
 WIN_COMBINATIONS = [
-top_column_win = [0,1,2], 
-middle_column_win = [3,4,5], 
-bottom_column_win = [6,7,8], 
-firstRow = [0,3,6], 
-secondRow = [1,4,7], 
-thirdRow = [2,5,8], 
-acrossedOne = [0,4,8], 
-acrossedTwo = [2,4,6], 
+  [0,1,2], 
+[3,4,5], 
+[6,7,8], 
+[0,3,6], 
+[1,4,7], 
+[0,4,8], 
+[2,4,6], 
+
   ]
