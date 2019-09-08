22d44af83c4af79545095eb5e838f4e9bbb1c56e
Javier Huerta Jr <javsteratii@gmail.com>
2019-09-08 18:48:23 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,20 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
+
+def won?
+end
+
+
+WIN_COMBINATIONS = [
+top_column_win = [0,1,2], 
+middle_column_win = [3,4,5], 
+bottom_column_win = [6,7,8], 
+firstRow = [0,3,6], 
+secondRow = [1,4,7], 
+thirdRow = [2,5,8], 
+acrossedOne = [0,4,8], 
+acrossedTwo = [2,4,6], 
+  ]
